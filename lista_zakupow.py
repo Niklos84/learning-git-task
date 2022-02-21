2f09b2a7bb4d5fde36360511170978867fa238b3
Mikolaj Strzelbicki <mikolaj.strzelbicki@gmail.com>
2022-02-21 13:40:50 +0100

    Add items sum

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -5,3 +5,10 @@ shopping_list = {
 
 for i in shopping_list:
     print(f'Idę do {i.capitalize()}, kupuję tu następujące rzeczy: {str(shopping_list[i]).title()}')
+
+count = 0
+
+for x,y in shopping_list.items():
+    count += len(y)
+
+print(f'W sumie kupuję {count} produktów.')
