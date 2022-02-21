760b90c4d046db671d8078a9b742fbe4249e103f
Mikolaj Strzelbicki <mikolaj.strzelbicki@gmail.com>
2022-02-21 13:32:16 +0100

    Add item print

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,6 @@ shopping_list = {
     'piekarnia' : ['chleb','bułki','pączek'],
     'warzywniak' : ['marchew','seler','rukola']
 }
+
+for i in shopping_list:
+    print(f'Idę do {i}, kupuję tu następujące rzeczy: {shopping_list[i]}')
